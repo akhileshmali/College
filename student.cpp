593d6a54afcd9c2f694fba1269190c5eec4ae273
Akhilesh <100013429+ZAKARTP@users.noreply.github.com>
2022-10-31 09:33:18 +0530

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -4,19 +4,18 @@ Name, Roll number, Class, division, Date of Birth, Blood group, Contact address,
 Construct the database with suitable member functions for initializing and destroying the data viz constructor, default constructor, Copy constructor, destructor, 
 static member functions, friend class, this pointer, inline code and dynamic memory allocation operators-new and delete as well as exception handling
 
-friend class, this pointer 
 
 */
 
 #include<iostream>
 #include<string.h>   //for handling string eg.strcpy()
 using namespace std;
+class info;
 class person
 {
 private:
   char *name,*blood,*address; //declared data variable
-  int dob;
-  long long int phone,license;
+  
   static int count;        //static variables
 public:
  person() //default & dynamic constructor
@@ -29,12 +28,9 @@ public:
  strcpy(blood,"B+ve");
  address=new char[20];
  strcpy(address,"Pune");
- phone=9911991111;
- license=123;
- dob=13012004;
  display();
 }
- person(char n[],char b[],int yy,char add[],long int p,long int ln)  //parameterized constructor
+ person(char n[],char b[],char add[])  //parameterized constructor
 {
   count++;
   int len;
@@ -49,9 +45,7 @@ public:
  len=strlen(add);
  address=new char[len+1];
  strcpy(address,add);
- dob=yy;
- phone=p;
-   license=ln;
+ 
 }
   void display();     //display function declaration
   
@@ -65,15 +59,40 @@ inline static int displaycount()  //inline static member function
 {
  cout<<"\n Destructor Called...";
 }
+friend class info;
 };
- 
+class info
+{
+private: 	
+ int dob;
+  long long int phone,license;
+public:	
+	info()
+	{
+		phone=9911991111;
+ 		license=123;
+ 		dob=13012004;
+ 		display1();
+	}
+	info(int dob,long int p,long int ln)  //parameterized constructor
+{
+   this->dob=dob;
+ phone=p;
+ license=ln;
+}
+void display1();
+ }; 
   int person::count=0;
          void person::display()  //display function definition
 {
  cout<<endl<<"\n NAME: "<<name;
  cout<<"\n BLOOD GROUP: "<<blood;
- cout<<"\n DATE OF BIRTH: "<<dob;
  cout<<"\n CONTACT ADDRESS: "<<address;
+ }
+
+void info::display1()  //display function definition
+{
+ cout<<"\n DATE OF BIRTH: "<<dob;
  cout<<"\n PHONE NUMBER: "<<phone;
  cout<<"\n LICENSE NUMBER: "<<license<<endl;
  }
@@ -82,6 +101,7 @@ int main()//initializing main function
 {
   int ch; //variable declaration
  person *p[20],*temp;
+ info *p1[20],*temp1;
  
   char pname[15],pblood[5],paddress[20];
   long int pphone,plicense;
@@ -100,7 +120,7 @@ int main()//initializing main function
 {
  case 1:  //DEFAULT CONSTRUCTER INFORMATION
  p[cnt]=new person();
- 
+ p1[cnt]=new info();
  cnt=cnt+1;
  break;
  case 2:  //PARAMETERIZED CONSTRUCTOR INFORMATION
@@ -110,13 +130,36 @@ int main()//initializing main function
  cin>>pblood;
  cout<<"\n ENTER DATE OF BIRTH :";
  cin>>dd;
+ /*do
+ {
+ 	cout<<"\n ENTER DATE OF BIRTH :";
+ cin>>dd;
+ try
+ {
+ 	if(sizeof(dd)>32)
+ 	{
+ 		cout<<" Enter valid date"<<endl;
+ 		throw "Error";
+	 }
+	 else
+	 {
+	 	break;
+	 }
+ }
+ catch(...)
+ {
+ 	cout<<"Enter valid date in DDMMYYYY format"<<endl;
+ }
+ 	
+ }while(true);*/
  cout<<"\n ENTER THE CONTACT ADDRESS:";
  cin>>paddress;
  cout<<"\n ENTER THE PHONE NUMBER:";
  cin>>pphone;
  cout<<"\n ENTER THE LICENSE NUMBER:";
  cin>>plicense;
- p[cnt]=new person(pname,pblood,dd,paddress,pphone,plicense);
+ p[cnt]=new person(pname,pblood,paddress);
+ p1[cnt]=new info(dd,pphone,plicense);
  
  
  cnt=cnt+1;
@@ -132,142 +175,13 @@ int main()//initializing main function
  for(int i=0;i<cnt;i++)
 {
  p[i]->display();
+ p1[i]->display1();
  }
+ 
  break; 
  case 5: 
           return 0;
 }
 } while(ch!=5);
 }
-
-/*
-/*
-Develop an object oriented program in C++ to create a database of student information system containing the following information: 
-Name, Roll number, Class, division, Date of Birth, Blood group, Contact address, telephone number, driving license no. and other. 
-Construct the database with suitable member functions for initializing and destroying the data viz constructor, default constructor, Copy constructor, destructor, 
-static member functions, friend class, this pointer, inline code and dynamic memory allocation operators-new and delete as well as exception handling
-*/
-
-//Name, Class,Roll number,  Date of Birth, Blood group,  string
-//division char
-
-//driving license no., Contact address, telephone number string MH-09-2008-0003369 
-#include<iostream>
-using namespace std;
-string name,clas,roll,dob,bg;
-class ot;
-class basic
-{
-	public:
-		basic()
-		{
-			name="Akhilesh";
-			clas="SE COMP 1";
-			roll="F21111006";
-			dob="13/01/2004";
-			bg="B+ve";
-		}
-	void input(ot*);
-	void show(ot*);
-	friend class ot;
-};
-string dl,ca,tn;
-class ot
-{
-	public:
-	ot()
-	{
-		dl="MH-14-2022-0003369";
-		ca="Chinchwad , Pune";
-		tn="+91 123456789";
-	}
-	void input1()
-	{
-		cout<<"Enter your Driving License No. :"<<endl;
-		getline(cin,dl);
-		cout<<"Enter your Contact Address :"<<endl;
-		getline(cin,ca);
-		cout<<"Enter your Telephone No. :"<<endl;
-		getline(cin,tn);
-	}
-	void show1()
-	{
-		cout<<"Driving License No. : "<<dl<<endl;
-		cout<<"Contact Address : "<<ca<<endl;
-		cout<<"Telephone No. : "<<tn<<endl;
-	}
-			
-};
-
-inline void basic::input(ot *st)
-{
-	cin.ignore();
-	cout<<"Enter your name :"<<endl;
-	getline(cin,name);
-	cout<<"Enter your class :"<<endl;
-	getline(cin,clas);
-	cout<<"Enter your Roll No :"<<endl;
-	getline(cin,roll);
-	cout<<"Enter your Date of Birth :"<<endl;
-	getline(cin,dob);
-	cout<<"Enter your Blood Group :"<<endl;
-	getline(cin,bg);
-	st->input1();
-}
-inline void basic::show(ot *st)
-{
-	cout<<"Name : "<<name<<endl;
-	cout<<"Class : "<<clas<<endl;
-	cout<<"Roll No. : "<<roll<<endl;
-	cout<<"Date of Birth : "<<dob<<endl;
-	cout<<"Blood Group : "<<bg<<endl;
-}
-int main()
-{
-	int n=0;
-	/*cout<<"Enter how many records to enter"<<endl;
-	cin>>n;
-	basic p1[n];*/
-	stud1[n]=new basic;
-    stud2[n]=new ot;
-	basic obj;
-	ot obj1;
-	
-	while(true)
-	{
-		int x;
-		cout<<"Enter choice 1. Display Data 2. Enter Data 3. Exit"<<endl;
-		cin>>x;
-		
-		switch(x)
-		{
-			case 1:{
-				/*obj.show();
-				obj1.show1();*/
-				for(int i=0;i<n;i++)
-   				{
-        			cout<<"---------------------------------------------------------------"<<endl;
-        			stud1[i]->show(stud2[i]);
-			    }
-				break;
-			}
-			case 2:{
-				//obj.input();
-				//p1[x].input(name,clas,roll,dob,bg);
-				//obj1.input1();
-				/*p1[x].input(dl,ca,tn);
-				x++;*/
-        		stud1[n]->input(stud2[n]);
-        		n++;
-				break;
-			}
-			case 3:
-				break;
-			default:
-				cout<<"Invalid"<<endl;	
-		}
-	}
-	return 0;
-}
-*/
 
